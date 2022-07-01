74504fb816059ecbc13027875ff9d9bbd34a8226
Maxime Herblot <maximeherblot123@gmail.com>
2022-07-01 16:37:00 +0200

    start search bar

--- src/components/home/dashboardVoyage.js
+++ src/components/home/dashboardVoyage.js
@@ -1,6 +1,49 @@
+import { useState } from "react";
 import CardVoyage from "./cardVoyage";
 
 let informationVoyage = [
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 20,
+    rating: 3.6,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 254,
+    rating: 4.2,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 354,
+    rating: 4.6,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 204,
+    rating: 3.6,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 20,
+    rating: 3.6,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 254,
+    rating: 4.2,
+  },
+  {
+    lieu: "Paris, France",
+    type: "Professionel",
+    tarif: 354,
+    rating: 4.6,
+  },
   {
     lieu: "Paris, France",
     type: "Professionel",
@@ -10,11 +53,20 @@ let informationVoyage = [
 ];
 
 const DashboardVoyage = () => {
+  const [listVoyage, setListVoyage] = useState(informationVoyage);
+
+  let handleOnChangeInput = (event)=> {
+    console.log(event.target.value)
+    setListVoyage(listVoyage)
+  }
   return (
     <>
-      {informationVoyage.map((information) => {
-        return <CardVoyage information={information} />;
-      })}
+      <input type="text" onChange={handleOnChangeInput}></input>
+      <div className="dashboardVoyage">
+        {informationVoyage.map((information) => {
+          return <CardVoyage information={information} />;
+        })}
+      </div>
     </>
   );
 };
---
+++ src/components/home/searchBar.js
@@ -0,0 +1,5 @@
+const searchBar = ()=> {
+
+}
+
+export default searchBar
