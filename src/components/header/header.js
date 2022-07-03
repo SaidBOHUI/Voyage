a946ba8eaa7ea12afe9b245f953cbc24b2e5a4a3
Saïd Bohui <said@macbook-pro-de-said-2.home>
2022-07-03 22:10:46 +0200

    pb liens

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,20 +12,21 @@ const Header = () => {
             </Link>
           </h3>
             <Liens>
+          {/* Normalement ce sont des liens mais on ne peut pas mettre de liens vide dans Netlify */}
               <Lien>
-                <Link to="/" >
-                  Sign In
-                </Link>
+                {/* <Link to="/" > */}
+                <p>Sign In</p>
+                {/* </Link> */}
               </Lien>
               <Lien>
-                <Link to="/" >
-                  Sign Up
-                </Link>
+                {/* <Link to="/" > */}
+                  <p>Sign Up</p>
+                {/* </Link> */}
               </Lien>
               <Lien>
-                <Link to="/" >
-                  Devenir Hôte
-                </Link>
+                {/* <Link to="/" > */}
+                  <p>Devenir Hôte</p>
+                {/* </Link> */}
               </Lien>
             </Liens>
         </Bar>
@@ -43,8 +44,13 @@ margin: 0 2rem;
 const Lien = styled.div`
 margin : 0 2rem;
 font-weight: bold;
-  &:hover{
-    color:green;
+  p{
+    color:white;
+    cursor: pointer;
+    &:hover{
+    color:#EFB85B;
+    transition: 1s;
+    }
   }
 `
 
